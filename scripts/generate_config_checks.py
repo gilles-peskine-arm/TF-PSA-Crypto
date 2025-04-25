3e3aeb6e1793daea2682217c961b302484f9155f
Gilles Peskine <Gilles.Peskine@arm.com>
2025-04-25 18:29:34 +0200

    Generate checks for bad options in the config file
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ scripts/generate_config_checks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Generate C preprocessor code to check for bad configurations.
+"""
+
+import framework_scripts_path # pylint: disable=unused-import
+from mbedtls_framework.config_checks_generator import * \
+    #pylint: disable=wildcard-import,unused-wildcard-import
+
+CRYPTO_CHECKS = BranchData(
+    header_directory='core',
+    header_prefix='tf_psa_crypto_',
+    project_cpp_prefix='TF_PSA_CRYPTO',
+    checkers=[
+        Internal('MBEDTLS_MD_SOME_LEGACY'),
+        Internal('MBEDTLS_MD_SOME_PSA'),
+        Removed('MBEDTLS_PADLOCK_C', '1.0'),
+        Removed('MBEDTLS_USE_PSA_CRYPTO', '1.0', 'none (always on)'),
+    ],
+)
+
+if __name__ == '__main__':
+    main(CRYPTO_CHECKS)
