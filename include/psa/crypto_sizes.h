dfad72972b5580078aba92e0c4a90b37484e8c4d
Gilles Peskine <Gilles.Peskine@arm.com>
2025-12-07 20:57:18 +0100

    Ascon-Hash256: add to the PSA API
    Add `PSA_ALG_ASCON_HASH256` to the PSA API: algorithm encoding, sizes,
    `MBEDTLS_PSA_ACCEL_` and `MBEDTLS_PSA_BUILTIN_` macros.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/psa/crypto_sizes.h
+++ include/psa/crypto_sizes.h
@@ -65,6 +65,7 @@
         PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_SHA3_256 ? 32u :      \
         PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_SHA3_384 ? 48u :      \
         PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_SHA3_512 ? 64u :      \
+        PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_ASCON_HASH256 ? 32u : \
         0u)
 
 /** The input block size of a hash algorithm, in bytes.
@@ -97,6 +98,7 @@
         PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_SHA3_256 ? 136u :     \
         PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_SHA3_384 ? 104u :     \
         PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_SHA3_512 ? 72u :      \
+        PSA_ALG_HMAC_GET_HASH(alg) == PSA_ALG_ASCON_HASH256 ? 64u : \
         0u)
 
 /** \def PSA_HASH_MAX_SIZE
