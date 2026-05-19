a7ef5bf1a03f8b4203459c89450f2fbc72d8c8c9
Gilles Peskine <Gilles.Peskine@arm.com>
2026-05-19 22:02:13 +0200

    PoC change to a global variable for the mldsa buffer offset
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- drivers/pqcp/src/psa_crypto_mldsa.c
+++ drivers/pqcp/src/psa_crypto_mldsa.c
@@ -10,6 +10,7 @@
 
 #include <psa/crypto.h>
 #include "psa_crypto_mldsa.h"
+#include "pqcp_buffer_alloc.h"
 #include "wrap_mldsa_native.h"
 #include <mbedtls/platform_util.h>
 
@@ -60,6 +61,10 @@ static psa_status_t seed_to_public_key(
         return PSA_ERROR_BUFFER_TOO_SMALL;
     }
 
+    psa_status_t status = tf_psa_crypto_pqcp_alloc_start();
+    if (status != 0) {
+        return status;
+    }
     /* Beyond this point, we must go through the cleanup code. */
     uint8_t secret[TF_PSA_CRYPTO_MLDSA_EXPANDED_SECRET_MAX_SIZE];
 
@@ -73,8 +78,13 @@ static psa_status_t seed_to_public_key(
     *data_length = public_key_length;
 
 cleanup:
+    status = tf_psa_crypto_pqcp_alloc_done();
     mbedtls_platform_zeroize(secret, sizeof(secret));
-    return pqcp_to_psa_error(ret);
+    if (status != PSA_SUCCESS) {
+        return status;
+    } else {
+        return pqcp_to_psa_error(ret);
+    }
 }
 
 psa_status_t tf_psa_crypto_mldsa_export_public_key(
@@ -123,7 +133,12 @@ psa_status_t tf_psa_crypto_mldsa_sign_message(
         return PSA_ERROR_BUFFER_TOO_SMALL;
     }
 
+    psa_status_t status = tf_psa_crypto_pqcp_alloc_start();
+    if (status != 0) {
+        return status;
+    }
     /* Beyond this point, we must go through the cleanup code. */
+
     uint8_t secret[TF_PSA_CRYPTO_MLDSA_EXPANDED_SECRET_MAX_SIZE];
     uint8_t public[TF_PSA_CRYPTO_MLDSA_PUBLIC_KEY_MAX_SIZE];
 
@@ -145,11 +160,15 @@ psa_status_t tf_psa_crypto_mldsa_sign_message(
                                                         rnd,
                                                         secret,
                                                         0);
-    ret = 0;
 
 cleanup:
+    status = tf_psa_crypto_pqcp_alloc_done();
     mbedtls_platform_zeroize(secret, sizeof(secret));
-    return pqcp_to_psa_error(ret);
+    if (status != PSA_SUCCESS) {
+        return status;
+    } else {
+        return pqcp_to_psa_error(ret);
+    }
 }
 
 psa_status_t tf_psa_crypto_mldsa_verify_message(
@@ -178,11 +197,20 @@ psa_status_t tf_psa_crypto_mldsa_verify_message(
         return PSA_ERROR_INVALID_SIGNATURE;
     }
 
+    psa_status_t status = tf_psa_crypto_pqcp_alloc_start();
+    if (status != 0) {
+        return status;
+    }
+
     int ret = tf_psa_crypto_pqcp_mldsa87_verify(signature, signature_length,
                                                 message, message_length,
                                                 NULL, 0,
                                                 key_buffer);
-    if (ret == 0) {
+
+    status = tf_psa_crypto_pqcp_alloc_done();
+    if (status != PSA_SUCCESS) {
+        return status;
+    } else if (ret == 0) {
         return PSA_SUCCESS;
     } else {
         /* At the time of writing, invalid signature is the only possible
